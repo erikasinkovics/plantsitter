1b420efa141be89e0e645b322a6570e28f82c2ea
Boran Zaza <boran.m.zaza@gmail.com>
2020-08-18 17:43:52 -0400

    My bookings controller seems to be functioning, and the views have a primitive version of what they should do

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :users do
+    resources :bookings, only: [:new, :create, :index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
