74e2b7ff5f18edb4db6a2514e47f2d295b6fa013
Boran Zaza <boran.m.zaza@gmail.com>
2020-08-18 12:10:31 -0400

    bookings model is added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_08_18_154949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "status"
+    t.bigint "customer_id"
+    t.bigint "plant_sitter_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_bookings_on_customer_id"
+    t.index ["plant_sitter_id"], name: "index_bookings_on_plant_sitter_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
+    t.string "postal_code"
+    t.text "bio"
+    t.boolean "plant_sitter"
+    t.boolean "pick_up"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bookings", "users", column: "customer_id"
+  add_foreign_key "bookings", "users", column: "plant_sitter_id"
 end
