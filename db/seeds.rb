10f6e7209ea673b8fa6a57a27baee003b70a19ae
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-08-20 12:37:05 -0400

    Changes to generated e-mail address and postal codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,14 @@ User.destroy_all
 
 puts "Creating customers..."
 
+postal_codes = ["H2H 1A1", "H2H 1A2", "H2H 1A3", "H2H 1A4", "H2H 1A5", "H2H 1A6", "H2H 1A7", "H2H 1A8", "H2H 1A9", "H2H 1B1"]
+
 5.times {
   attributes = {
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    postal_code: "H2H",
+    email: "#{Faker::Name.first_name.downcase}.#{Faker::Name.last_name.downcase}@plantlover.com",
+    postal_code: postal_codes.sample,
     password: "password",
     bio: Faker::Games::LeagueOfLegends.quote,
     plant_sitter: false,
@@ -29,7 +31,7 @@ User.create!({
     first_name: "Karine",
     last_name: Faker::Name.last_name,
     email: "karine@gmail.com",
-    postal_code: "H2H",
+    postal_code: "H2H 1C1",
     password: "password",
     bio: "Hi! I'm Karine. I'm a 28 years old plant enthusiast from Montreal, proud plant-mom of 15.",
     plant_sitter: false,
@@ -41,8 +43,8 @@ puts "Creating plant sitters..."
   attributes = {
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    postal_code: "H2H",
+    email: "#{Faker::Name.first_name.downcase}.#{Faker::Name.last_name.downcase}@plantlover.com",
+    postal_code: postal_codes.sample,
     password: "password",
     bio: Faker::Games::LeagueOfLegends.quote,
     plant_sitter: true,
